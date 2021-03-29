78b620ec01010152a6a952936c40a278ff0870f6
Afifahkr <75968800+afifahkr@users.noreply.github.com>
2021-03-29 07:37:22 +0700

    Add files via upload

---
+++ Aplikasii.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aplikasii;
+
+/**
+ *
+ * @author lenovo
+ */
+public class Aplikasii {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Loginvieww lv = new Loginvieww();
+        lv.setLocationRelativeTo(null);
+        lv.setVisible(true);
+    }
+    
+}
